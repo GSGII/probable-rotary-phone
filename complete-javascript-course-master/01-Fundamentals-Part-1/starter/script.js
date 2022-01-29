99de86a34d40b4eafc97fd71a51d400ca8279dca
Gregory S Gentry II <gregoryscottg@outlook.com>
2022-01-28 21:16:39 -0600

    Operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -94,6 +94,18 @@ x--; // x = x -1
 x--;
 console.log(x);
 
+// Comparison Operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+
+
+
 
 
 
