4dae6f57e929571e8476d2d3e1c5b3256f683502
Gregory S Gentry II <gregoryscottg@outlook.com>
2022-01-28 20:08:04 -0600

    practicum adding

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/practicum.js
@@ -0,0 +1,13 @@
+let country = 'USA';
+let continent = 'North America';
+let population = '300 Million EST';
+      
+let isIsland = "False"
+let language;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
+
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,14 +1,78 @@
-let country = 'USA';
-let continent = 'North America';
-let population = '300 Million EST';
+      let js = "amazing";    
+      /*console.log(40 + 8 + 23 - 10);
 
-console.log(country);
-console.log(continent);
-console.log(population);
+      console.log('Jonas');
+      console.log(23);
+
+      let firstName = "Matilda";
+       
+
+      console.log(firstName);
+      console.log(firstName);
+      console.log(firstName);
+
+
+      //variable name conventions
+      let jonas_matilda = 'jm'
+      let $function = 27;
+
+      let person = 'jonas'
+      let PI = 3.1415;
+
+      let myFirstJob = 'Programer'
+      let myCurrentJob = 'Teacher'
+      console.log(myFirstJob);
+      console.log(myCurrentJob);
+      */
+
+      /*
+      let country = 'USA';
+      let continent = 'North America';
+      let population = '300 Million EST';
+      
+      console.log(country);
+      console.log(continent);
+      console.log(population);
+      */
+
+      let javascriptIsFun = true;
+      console.log(javascriptIsFun);
+
+      // console.log(typeof true);
+      console.log(typeof javascriptIsFun);
+      // console.log(typeof 23);
+      // console.log(typeof 'jonas');
+
+      javascriptIsFun = 'YES!';
+      console.log(typeof javascriptIsFun);
+
+      let year;
+      console.log(year);
+      console.log(typeof year);
+
+      year = 1991;
+      console.log(year);
+      console.log(typeof year);
+
+      console.log(typeof null);
 
 
 
-     
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
 
 
 
