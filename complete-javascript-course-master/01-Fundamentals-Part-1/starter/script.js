04cc2c8c6df1085d3fa99e09dd6760a31f5092e2
Gregory S Gentry II <gregoryscottg@outlook.com>
2022-01-28 20:14:21 -0600

    new updated

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,60 +1,70 @@
-      let js = "amazing";    
-      /*console.log(40 + 8 + 23 - 10);
+let js = "amazing";    
+/*console.log(40 + 8 + 23 - 10);
 
-      console.log('Jonas');
-      console.log(23);
+console.log('Jonas');
+console.log(23);
 
-      let firstName = "Matilda";
+let firstName = "Matilda";
        
 
-      console.log(firstName);
-      console.log(firstName);
-      console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
 
 
-      //variable name conventions
-      let jonas_matilda = 'jm'
-      let $function = 27;
+//variable name conventions
+let jonas_matilda = 'jm'
+let $function = 27;
 
-      let person = 'jonas'
-      let PI = 3.1415;
+let person = 'jonas'
+let PI = 3.1415;
 
-      let myFirstJob = 'Programer'
-      let myCurrentJob = 'Teacher'
-      console.log(myFirstJob);
-      console.log(myCurrentJob);
-      */
+let myFirstJob = 'Programer'
+let myCurrentJob = 'Teacher'
+console.log(myFirstJob);
+console.log(myCurrentJob);
+*/
 
-      /*
-      let country = 'USA';
-      let continent = 'North America';
-      let population = '300 Million EST';
+/*
+let country = 'USA';
+let continent = 'North America';
+let population = '300 Million EST';
       
-      console.log(country);
-      console.log(continent);
-      console.log(population);
-      */
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
 
-      let javascriptIsFun = true;
-      console.log(javascriptIsFun);
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'jonas');
 
-      // console.log(typeof true);
-      console.log(typeof javascriptIsFun);
-      // console.log(typeof 23);
-      // console.log(typeof 'jonas');
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
 
-      javascriptIsFun = 'YES!';
-      console.log(typeof javascriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
 
-      let year;
-      console.log(year);
-      console.log(typeof year);
+year = 1991;
+console.log(year);
+console.log(typeof year);
 
-      year = 1991;
-      console.log(year);
-      console.log(typeof year);
+console.log(typeof null);
+*/
 
-      console.log(typeof null);
+let age = 30;
+age = 31;
+      
+const birthyear = 1991;
+// birthyear = 1990;
+     
+const job;
+
 
 
 
