41db106610351602609720a4570c401234fba96f
Gregory S Gentry II <gregoryscottg@outlook.com>
2022-01-28 19:13:00 -0600

    e

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,13 @@
-
-      let js = "amazing";
-      if (js === "amazing") alert("Javascript is fun!");
-      40 + 8 + 23 - 10;
+      let js = "amazing";    
       console.log(40 + 8 + 23 - 10);
+
+      console.log('Jonas');
+      console.log(23);
+
+      let firstName = "Matilda";
+      console.log(firstName)
+      console.log(firstName)
+      console.log(firstName)
+
+
+
