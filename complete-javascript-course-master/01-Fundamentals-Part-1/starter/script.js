425e32197a5475f3fbaa0203ada856294a2ed143
Gregory S Gentry II <gregoryscottg@outlook.com>
2022-01-28 21:06:44 -0600

    assignment operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -57,7 +57,7 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
-let age = 30;
+/*let age = 30;
 age = 31;
       
 const birthyear = 1991;
@@ -69,6 +69,39 @@ job = 'teacher'
 
 lastName = 'Gentry'
 console.log(lastName);
+*/
+
+// Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas,ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 
+
+const firstName = 'Greg';
+const lastName = 'Gentry';
+
+console.log(firstName + ' ' + lastName);
+
+// assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; //x = x + 1
+x--; // x = x -1
+x--;
+console.log(x);
+
+
+
+
+
+
+
+
+
 
 
 
