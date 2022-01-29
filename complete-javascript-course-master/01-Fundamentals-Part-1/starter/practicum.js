920264c8eeb01f241c6bbdf9cd346db19a3f3b38
Gregory S Gentry II <gregoryscottg@outlook.com>
2022-01-28 22:01:18 -0600

    new

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/practicum.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/practicum.js
@@ -1,9 +1,9 @@
 let country = 'USA';
 let continent = 'North America';
-let population = '300 Million EST';
+let population = '300000000';
       
 let isIsland = "False"
-let language;
+let language = 'english';
 
 console.log(country);
 console.log(continent);
@@ -11,8 +11,20 @@ console.log(population);
 console.log(isIsland);
 console.log(language);
 
-country = 'Mexico';
-language = 'Spanish';
 
-console.log(country);
-console.log(language);
+
+// this is going to take the object of population and use operators to set a new object called new operation and use that in the new math operation.
+let newPopulation = population / 2 / 2;
+console.log(newPopulation);
+
+// If I use a plus sign here with an integer, it will add it as a new place after the full result
+newPopulation++ ;
+console.log(newPopulation);
+
+let finland = 6000000
+
+console.log(newPopulation < finland);
+console.log(newPopulation > 3300000);
+
+let description = 'USA is in North America and its 300000000 people speak English';
+console.log(description);
